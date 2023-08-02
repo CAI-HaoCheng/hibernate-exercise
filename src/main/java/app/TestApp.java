181f43b95e938483b63263d5e1424601b55a79ce
HiddenMW <51083911+CAI-HaoCheng@users.noreply.github.com>
2023-08-02 18:20:50 +0800

    Hibernate 新增、刪除練習

--- src/main/java/app/TestApp.java
+++ src/main/java/app/TestApp.java
@@ -2,17 +2,55 @@ package app;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.mysql.cj.x.protobuf.MysqlxCrud.Insert;
 
 import core.util.HibernateUtil;
 import web.member.pojo.Member;
 
 public class TestApp {
 	public static void main(String[] args) {
+//		Member member = new Member();
+//		member.setUsername("使用者名稱");
+//		member.setPassword("密碼");
+//		member.setNickname("暱稱");
+		TestApp testApp = new TestApp();
+//		testApp.insert(member);
+		System.out.println(testApp.deleteById(5));
+		
+	}
+
+	public Integer insert(Member member) {
 		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 		Session session = sessionFactory.openSession();
-		Member member = session.get(Member.class, 1);
-		System.out.println(member.getNickname());
-		HibernateUtil.shutdown();
-
+		try {
+			Transaction transaction = session.beginTransaction();
+			session.persist(member);
+			transaction.commit();
+			return member.getId();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			return null;
+		}
+	}
+	
+	public int deleteById(Integer id) {
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		try {
+			Transaction transaction = session.beginTransaction();
+			Member member = session.get(Member.class, id);
+			session.remove(member);
+			transaction.commit();
+			return 1;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			return -1;
+		}
 	}
 }
