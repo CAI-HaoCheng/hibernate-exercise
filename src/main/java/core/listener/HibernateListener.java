5139881096588d8781d0abc5f67f623d7faac398
HiddenMW <51083911+CAI-HaoCheng@users.noreply.github.com>
2023-08-06 09:02:36 +0800

    HIbernate SQL設定寫法

---
+++ src/main/java/core/listener/HibernateListener.java
@@ -0,0 +1,19 @@
+package core.listener;
+
+import static core.util.HibernateUtil.*;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+
+@WebListener
+public class HibernateListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        getSessionFactory();
+    }
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        shutdown();
+    }
+}
--- src/main/java/core/pojo/Core.java
+++ src/main/java/core/pojo/Core.java
@@ -2,7 +2,10 @@ package core.pojo;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+
 import lombok.Data;
+
 @Data
 public class Core implements Serializable {
 	private static final long serialVersionUID = 1457755989409740329L;
